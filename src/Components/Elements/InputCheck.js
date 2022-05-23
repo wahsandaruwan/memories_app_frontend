4ba3a883ebc379e856b7336b2a56eae51734fcb9
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-05-24 04:15:46 +0530

    Added remember e check box

---
+++ src/Components/Elements/InputCheck.js
@@ -0,0 +1,34 @@
+// Custom modules
+import { PropTypes } from "prop-types"
+
+const InputCheck = ({ checkBoxId, checkBoxValue, changeFunc }) => {
+    return (
+        <>
+            <input id={checkBoxId} className="check-custom" style={basicStyles} type="checkbox" value={checkBoxValue} onChange={changeFunc} />
+        </>
+    )
+}
+
+// Basic styling
+const basicStyles = {
+    position: "relative",
+    width: "18px",
+    height: "18px",
+    padding: "0",
+    margin: "5px"
+}
+
+// Type checking
+InputCheck.propTypes = {
+    checkBoxId: PropTypes.string,
+    checkBoxValue: PropTypes.string,
+    changeFunc: PropTypes.func.isRequired
+}
+
+// Default values
+InputCheck.defaultProps = {
+    checkBoxValue: "value",
+    changeFunc: () => console.log("Checking")
+}
+
+export default InputCheck
--- src/Components/Sections/Login.js
+++ src/Components/Sections/Login.js
@@ -1,5 +1,6 @@
 // Custom components
 import InputBox from "../Elements/InputBox"
+import InputCheck from "../Elements/InputCheck"
 import MainButton from "../Elements/MainButton"
 
 const Login = () => {
@@ -10,6 +11,10 @@ const Login = () => {
                     <h1 className="section-heading">Login</h1>
                     <InputBox inputType="text" inputPlaceholder="Enter Your Username..." />
                     <InputBox inputType="password" inputPlaceholder="Enter Your Password..." />
+                    <div className="input-check">
+                        <InputCheck checkBoxId="agree-box" checkBoxValue="agree" />
+                        <label htmlFor="agree-box">Remember Me</label>
+                    </div>
                     <MainButton buttonText="Login" />
                     <a className="to-reg" href="#">Create a New Account</a>
                 </div>
