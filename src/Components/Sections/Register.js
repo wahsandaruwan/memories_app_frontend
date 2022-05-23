c38c50cad300c16702fd82eaffdfb09aa0f50b24
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-05-24 04:22:10 +0530

    Created register form

--- src/Components/Sections/Register.js
+++ src/Components/Sections/Register.js
@@ -3,24 +3,27 @@ import InputBox from "../Elements/InputBox"
 import InputCheck from "../Elements/InputCheck"
 import MainButton from "../Elements/MainButton"
 
-const Login = () => {
+const Register = () => {
     return (
         <>
             <div className="login-register-section">
                 <div className="login-register-form">
-                    <h1 className="section-heading">Login</h1>
+                    <h1 className="section-heading">Register</h1>
+                    <InputBox inputType="text" inputPlaceholder="Enter Your Full Name..." />
+                    <InputBox inputType="text" inputPlaceholder="Enter Your Email..." />
                     <InputBox inputType="text" inputPlaceholder="Enter Your Username..." />
                     <InputBox inputType="password" inputPlaceholder="Enter Your Password..." />
+                    <InputBox inputType="password" inputPlaceholder="Re-Enter Your Password..." />
                     <div className="input-check">
                         <InputCheck checkBoxId="agree-box" checkBoxValue="agree" />
-                        <label htmlFor="agree-box">Remember Me</label>
+                        <label htmlFor="agree-box">Agree <span>Terms & Conditions</span></label>
                     </div>
                     <MainButton buttonText="Login" />
-                    <a className="to-reg" href="#">Create a New Account</a>
+                    <a className="to-reg" href="#">Have an Account</a>
                 </div>
             </div>
         </>
     )
 }
 
-export default Login
+export default Register
